89b606f8aca2c7f030e955001929e8506bb64891
mdrzzz24 <achmadrozan24@gmail.com>
2023-01-07 07:18:10 +0700

    Add Multi User and Auth full system

--- LokerkuID/app/Http/Controllers/HomeController.php
+++ LokerkuID/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        if(Auth::check()){
+            if(Auth::user()->level == 0){
+                return redirect('admin');
+            }elseif(Auth::user()->level == 1){
+                return redirect('company');
+            }else{
+                return redirect('user');
+            }
+        }
     }
 }
--- LokerkuID/app/Http/Kernel.php
+++ LokerkuID/app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
+        'isCom' => \App\Http\Middleware\CompanyMiddleware::class,
+        'isUser' => \App\Http\Middleware\UserMiddleware::class,
     ];
 }
